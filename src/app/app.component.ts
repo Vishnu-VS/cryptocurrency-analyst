732b467a22e16a5cec76e9518861a2e0a2756b5b
Vishnu V S <vishnu.vs@yahoo.com>
2020-11-23 20:11:54 +0530

    trending crypto coins list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit,  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'cryptocurrency-analyst';
+
+  coinsTrending: any;
+  constructor(private http: HttpClient, private changeDetectorRef: ChangeDetectorRef) { }
+
+  ngOnInit(){
+    this.http.get('https://api.coingecko.com/api/v3/ping').subscribe((res)=>{
+      console.log(res);
+    });
+    this.http.get('https://api.coingecko.com/api/v3/coins/list').subscribe((res)=>{
+      // console.log(res);
+    });
+    this.http.get('https://api.coingecko.com/api/v3/search/trending').subscribe((res)=>{
+      this.coinsTrending = res;
+      // for(let i=0;i<this.coinsTrending.coins.length; i++){
+      //   console.table(this.coinsTrending.coins[i]);
+      // }
+      console.log(this.coinsTrending.coins);
+      this.changeDetectorRef.detectChanges();
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
