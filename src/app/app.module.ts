0c4dc65a66ef969a350b937a4061c11d0f9bb804
Vishnu V S <vishnu.vs@yahoo.com>
2020-11-29 13:43:38 +0530

    show graph of one bitcoin at a time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 import { NgxEchartsModule } from 'ngx-echarts';
 import { SwiperModule } from 'ngx-swiper-wrapper';
@@ -46,6 +47,7 @@ import { CompareComponent } from './compare/compare.component';
     MatInputModule,
     MatSelectModule,
     MatAutocompleteModule,
+    MatButtonModule,
     NgxEchartsModule.forRoot({
       /**
        * This will import all modules from echarts.
--- src/app/compare/compare.component.ts
+++ src/app/compare/compare.component.ts
@@ -3,12 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { EChartOption } from 'echarts';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
-import {FormControl} from '@angular/forms';
+import {FormGroup, FormControl} from '@angular/forms';
 
 export interface Coins{
   id: string,
   name: string,
   symbol: string
+}
+
+export interface MarketChartParameters{
+  id: string,
+  vs_currency: string,
+  days: string
 }
 
 @Component({
@@ -21,11 +27,98 @@ export class CompareComponent implements OnInit {
   chartOption: EChartOption;
   supportedCurrencies: any;
   currencyDropdown: any;
-  myControl = new FormControl();
+  selectedCoin = new FormControl('bitcoin');
+  selectedCurrency= new FormControl('USD');
+  selectedDays= new FormControl('30');
   options: Coins[];
   filteredOptions: Observable<Coins[]>;
+  marketChartParams: MarketChartParameters[];
+  chartOptionGrouped: any[] = [];
+  marketchartPrices: any[] = [];
+  fetchCoinData = new FormGroup({
+    selectedCoin: new FormControl('')
+  });
 
   constructor(private http: HttpClient) {}
+
+  marketChartApi(marketChartParams: MarketChartParameters) {
+    return this.http
+      .get(
+        'https://api.coingecko.com/api/v3/coins/' +
+          marketChartParams.id +
+          '/market_chart?vs_currency='+marketChartParams.vs_currency+'&days='+marketChartParams.days+'&interval=daily'
+      )
+      .toPromise();
+  }
+
+  async getMarketChart(marketChartParams: MarketChartParameters[]) {
+    // console.log(coins);
+    for (let i = 0; i < marketChartParams.length; i++) {
+      var marketcharttemp = await this.marketChartApi(marketChartParams[i]);
+      // console.log(i);
+      // console.log(marketcharttemp);
+      let marketchart: any = marketcharttemp;
+      let maxPrice: any;
+      let minPrice: any;
+
+      for (let j = 0; j < marketchart.prices.length; j++) {
+        if (j == 0) {
+          maxPrice = marketchart.prices[j][1];
+          minPrice = marketchart.prices[j][1];
+        } else {
+          if (maxPrice < marketchart.prices[j][1]) {
+            maxPrice = marketchart.prices[j][1];
+          }
+          if (minPrice > marketchart.prices[j][1]) {
+            minPrice = marketchart.prices[j][1];
+          }
+        }
+        //this.prices.push(marketchart.prices[j][1]);
+        this.marketchartPrices.push(marketchart.prices[j][1]);
+      }
+      // console.log(this.marketchartPrices);
+      // console.log(maxPrice);
+      // console.log(minPrice);
+      this.chartOption = {
+        // title: {
+        //   text: this.coinsTrending.coins[i].item.id,
+        //   show: true,
+        // },
+        xAxis: {
+          type: 'category',
+          splitLine: {
+            show: true,
+          },
+          show: true,
+        },
+        yAxis: {
+          show: true,
+          type: 'value',
+          // min: minPrice,
+          // max: maxPrice,
+          splitLine: {
+            show: true,
+          },
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        series: [
+          {
+            data: this.marketchartPrices,
+            type: 'line',
+            animationEasing: 'linear',
+            animationDuration: 1000,
+            showSymbol: true,
+            hoverAnimation: true,
+          },
+        ],
+      };
+      this.chartOptionGrouped.push(this.chartOption);
+      this.marketchartPrices = [];
+    }
+    console.log(this.chartOptionGrouped);
+  }
 
   ngOnInit(): void {
     this.http
@@ -34,14 +127,14 @@ export class CompareComponent implements OnInit {
         for (var i = 0; i < this.supportedCurrencies.length; i++) {
           this.supportedCurrencies[i] = this.supportedCurrencies[i].toUpperCase();
         }
-        console.log(this.supportedCurrencies);
+        // console.log(this.supportedCurrencies);
       }
     );
     this.http.get("https://api.coingecko.com/api/v3/coins/list").subscribe(res => {
-      console.log(res);
+      // console.log(res);
       this.options = <Coins[]>res;
     });
-    this.filteredOptions = this.myControl.valueChanges
+    this.filteredOptions = this.selectedCoin.valueChanges
       .pipe(
         startWith(''),
         map(value =>
@@ -49,6 +142,27 @@ export class CompareComponent implements OnInit {
           )
       );
   }
+
+  fetchCoinPrice(){
+    console.log(this.selectedCoin.value);
+    console.log(this.selectedCurrency.value);
+    console.log(this.selectedDays.value);
+    this.marketChartParams = [{
+      id: this.selectedCoin.value,
+      vs_currency: this.selectedCurrency.value,
+      days: this.selectedDays.value
+    }];
+    // this.http
+    //     .get(
+    //       'https://api.coingecko.com/api/v3/coins/' +
+    //       this.selectedCoin.value +
+    //         '/market_chart?vs_currency='+this.selectedCurrency.value+'&days='+this.selectedDays.value+'&interval=daily'
+    //     ).subscribe(res => {
+    //       console.log(res);
+    //     });
+    this.getMarketChart(this.marketChartParams);
+  }
+
   private _filter(value: string): Coins[] {
     if(value.length >2){
       const filterValue = value.toLowerCase();
